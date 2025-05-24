d0690590a110f4cb964aced3df7089d30e9cad2a
meichuhe <1532744860@qq.com>
2025-05-24 21:52:37 +0200

    first commit

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+# from app.views import home, ArticleCreateView
+from app.views import ArticleListView, ArticleUpdateView, ArticleDeleteView, ArticleCreateView
+
+urlpatterns = [
+    # path("", home, name="home"),
+    # path("articles/create/", create_article, name="create_article")
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("", ArticleListView.as_view(), name="home"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"),
+]
---
+++ app/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from app.models import Article
+from app.forms import CreateArticleForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
+def home(request):
+    articles = Article.objects.all()
+    return render(request, "app/home.html", {"articles": articles})
+
+def create_article(request):
+    if request.method == "POST":
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            new_article = Article(
+                title=form_data["title"],
+                status=form_data["status"],
+                content=form_data["content"],
+                word_count=form_data["word_count"],
+                twitter_post=form_data["twitter_post"],
+            )
+            new_article.save()
+
+            return redirect("home")
+    else:
+        form = CreateArticleForm()
+    return render(request, "app/article_create.html", {"form": form})
+
+
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
+
+
+class ArticleCreateView(CreateView):
+    template_name = "app/article_create.html"
+    model = Article
+    fields = ["title", "status", "content", "twitter_post"]
+    success_url = reverse_lazy("home")
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "twitter_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
+
+
