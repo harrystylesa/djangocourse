54edf799fd7d66b6ac16ab916cfcb665dbf0b1f2
meichuhe <1532744860@qq.com>
2025-05-26 21:55:21 +0200

    add OAuth

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from app.models import Article
 from app.forms import CreateArticleForm
 from django.urls import reverse_lazy
@@ -32,13 +36,16 @@ def create_article(request):
     return render(request, "app/article_create.html", {"form": form})
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
 
+    def get_queryset(self) -> QuerySet[Any]:
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
@@ -49,16 +56,22 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "app/article_update.html"
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     success_url = reverse_lazy("home")
     context_object_name = "article"
 
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "app/article_delete.html"
     model = Article
     success_url = reverse_lazy("home")
     context_object_name = "article"
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
